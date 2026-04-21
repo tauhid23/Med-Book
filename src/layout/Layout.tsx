d88ed38d1a092b5d6b6beeef95d4e3ddc5871fda
tauhid23 <tauhidclover@gmail.com>
2026-04-21 16:02:19 +0600

    Apr21

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
+import ScrollToTop from "../components/ScrollTop";
 
 export default function Layout() {
     return (
         <div className="flex flex-col min-h-screen bg-bg text-text">
+            <ScrollToTop/>
             {/* ── Navbar ── */}
             <Navbar />
 
